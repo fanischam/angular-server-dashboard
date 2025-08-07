579f4386f57639afbdb1be28dd2bbb568fad9156
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-08-07 18:27:49 +0300

    added logic to change server status

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,14 @@ import { Component, input } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unkown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      this.currentStatus =
+        rnd < 0.5 ? 'online' : rnd < 0.8 ? 'offline' : 'unkown';
+    }, 3000);
+  }
 }
