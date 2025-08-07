cd2f11eb8ec5595bc21ed76284822eaf4b0f4cf9
Fanis Chamalidis <fchamalidis@yahoo.com>
2025-08-07 13:19:00 +0300

    refactored button to project elements based on nested attributes

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -7,7 +7,4 @@ import { Component, input } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
 })
-export class ButtonComponent {
-  text = input.required<string>();
-  icon = input.required<string>();
-}
+export class ButtonComponent {}
